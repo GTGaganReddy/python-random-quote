c59eef20322271f55c3388b3771d9d9c230fb629
GTGaganReddy <74946644+GTGaganReddy@users.noreply.github.com>
2020-11-25 16:23:56 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
+import random
 def main():
-  # print("Keep it logically awesome.")
-
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  #print("Keep it logically awesome.")
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  rand=random.randint(0, 13)
+  print(quotes[rand])
 
 if __name__== "__main__":
   main()
